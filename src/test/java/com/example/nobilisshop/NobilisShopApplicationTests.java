ed09ec7352a9403fc564aaca0912397d8be9cb4c
Patkamil <patkamil@o2.pl>
2023-12-08 18:10:44 +0100

    Empty project

---
+++ src/test/java/com/example/nobilisshop/NobilisShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nobilisshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobilisShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
