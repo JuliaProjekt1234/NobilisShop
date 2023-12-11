66cdb296a0000495d67582dec345432fc44db364
Patkamil <patkamil@o2.pl>
2023-12-11 19:25:17 +0100

    Działająca wersja

--- src/main/java/com/example/nobilisshop/NobilisShopApplication.java
+++ src/main/java/com/example/nobilisshop/NobilisShopApplication.java
@@ -2,11 +2,15 @@ package com.example.nobilisshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.nobilisshop.repository")
 public class NobilisShopApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NobilisShopApplication.class, args);
     }
 
